9af3b69449d3468f6ff9e85830890ab61344c859
Oussama BASRY <58572674+oussamabasry@users.noreply.github.com>
2021-04-22 16:14:17 +0000

    change

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import * as mdb from "mdb-ui-kit";
 
 ReactDOM.render(
   <React.StrictMode>
